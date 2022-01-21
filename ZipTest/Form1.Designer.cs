762f6fd38a36ee65a172994d8250828276f7f578
Samet CETIN <sametcetin@yandex.com>
2022-01-21 11:43:49 +0300

    load project to git

---
+++ ZipTest/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace ZipTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.btnUnzip = new System.Windows.Forms.Button();
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(27, 32);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "Select File";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // btnUnzip
+            // 
+            this.btnUnzip.Location = new System.Drawing.Point(27, 82);
+            this.btnUnzip.Name = "btnUnzip";
+            this.btnUnzip.Size = new System.Drawing.Size(75, 23);
+            this.btnUnzip.TabIndex = 1;
+            this.btnUnzip.Text = "Unzip";
+            this.btnUnzip.UseVisualStyleBackColor = true;
+            this.btnUnzip.Click += new System.EventHandler(this.btnUnzip_Click);
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.AutoSize = true;
+            this.lblFilePath.Location = new System.Drawing.Point(126, 37);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(45, 13);
+            this.lblFilePath.TabIndex = 2;
+            this.lblFilePath.Text = "FilePath";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblFilePath);
+            this.Controls.Add(this.btnUnzip);
+            this.Controls.Add(this.btnSelectFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Button btnUnzip;
+        private System.Windows.Forms.Label lblFilePath;
+    }
+}
+
---
+++ ZipTest/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZipTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            lblFilePath.Text = "FilePath";
+
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Filter = "(*.zip) | *.zip";
+            DialogResult dr = opf.ShowDialog();
+            if (dr != DialogResult.OK)
+                return;
+
+            lblFilePath.Text = opf.FileName;
+        }
+
+        private void btnUnzip_Click(object sender, EventArgs e)
+        {
+            if (lblFilePath.Text == "FilePath")
+                return;
+
+            var unzipPath = "D:\\UnzippedFile\\";
+
+            if (Directory.Exists(unzipPath))
+            {
+                try
+                {
+                    DirectoryInfo di = new DirectoryInfo(unzipPath);
+
+                    foreach (FileInfo file in di.GetFiles())
+                    {
+                        file.Delete();
+                    }
+                    foreach (DirectoryInfo dir in di.GetDirectories())
+                    {
+                        dir.Delete(true);
+                    }
+
+                    Directory.Delete(unzipPath);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+            }
+
+            try
+            {
+                System.IO.Compression.ZipFile.ExtractToDirectory(lblFilePath.Text, unzipPath);
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
